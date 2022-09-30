d0fae4cbad1f0f60cc168627cccaddcf1dafe74f
Juan Agustin Apaza Guzman <juanagustin7887@gmail.com>
2022-09-29 22:49:00 -0400

    First commit

---
+++ principal.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<conio.h>
+#include<string.h>
+
+using namespace std;
+
+int main() {
+
+	cout << "**********************  HOLA MUNDO  **********************" << endl;
+	cout << "\nNombre y Apellido: Juan Agustin Apaza Guzman" << endl;
+    cout << "Correo: juanagustin7887@gmail.com" << endl;
+	cout << "Telefono: 78790716" << endl;
+	cout << "Fecha de nacimiento: 11 de julio de 2001" << endl;
+	cout << "C.I: 9071069" << endl;
+    cout << "Estado civil: Soltero" << endl;
+	cout << "Desafio: Este 2022/2 pienso dar todo de mi este semestre y me comprometo " << endl;
+	cout << "a pasar este curso y que no me boya aplazar, haci que comenzamos con todas las ganas..." << endl;
+	cout << "\n********************** SOY UN SIMPLE ESTUDIANTE :) **********************" << endl;
+
+	return 0;
+}
